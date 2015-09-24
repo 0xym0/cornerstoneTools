e5297b432820ef631e4644edcb0d10e64cf73c91
Erik Ziegler <erik.ziegler@ulg.ac.be>
2015-09-24 13:48:11 +0200

    Create CornerstoneToolsTouchEnd event. Moved magnifyTouch away from touch point and added options to touchDragTool

--- src/imageTools/touchDragTool.js
+++ src/imageTools/touchDragTool.js
@@ -2,21 +2,33 @@
 
     'use strict';
 
-    function touchDragTool(touchDragCallback, fireOnTouchStart) {
+    function touchDragTool(touchDragCallback, options) {
         var events = 'CornerstoneToolsTouchDrag';
-        if (fireOnTouchStart === true) {
+        if (options && options.fireOnTouchStart === true) {
             events += ' CornerstoneToolsTouchStart';
         }
         var toolInterface = {
             activate: function(element) {
                 $(element).off(events, touchDragCallback);
                 $(element).on(events, touchDragCallback);
+                if (options && options.activateCallback) {
+                    options.activateCallback(element);
+                }
             }, disable: function(element) {
                 $(element).off(events, touchDragCallback);
+                if (options && options.disableCallback) {
+                    options.disableCallback(element);
+                }
             }, enable: function(element) {
                 $(element).off(events, touchDragCallback);
+                if (options && options.enableCallback) {
+                    options.enableCallback(element);
+                }
             }, deactivate: function(element) {
                 $(element).off(events, touchDragCallback);
+                if (options && options.deactivateCallback) {
+                    options.deactivateCallback(element);
+                }
             }
         };
         return toolInterface;
